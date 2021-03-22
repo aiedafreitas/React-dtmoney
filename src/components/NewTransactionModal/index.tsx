99f41837a904c1b1312466710f6515ef8025f5fd
Aieda <aieda@arabella.ag>
2021-03-22 11:38:20 -0300

    tipos botões

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
-import { Container } from './style';
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import { Container, TransactionTypeContainer } from './style';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -33,6 +35,24 @@ placeholder="Título"/>
 <input 
 type="number"
 placeholder="Valor  "/>
+
+<TransactionTypeContainer>
+    <button 
+    type="button">
+    <img src={incomeImg} alt="Entrada" />
+    <span>Entrada</span>
+    </button>
+
+    <button 
+    type="button">
+    <img src={outcomeImg} alt="Saída" />
+    <span>Saída</span>
+    </button>
+    
+</TransactionTypeContainer>
+
+
+
 
 <input 
 placeholder="Categoria"/>
