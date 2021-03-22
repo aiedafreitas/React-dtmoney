43b455fe502e35c1ec5cb386936b4d1e0aabc610
Aieda <aieda@arabella.ag>
2021-03-22 11:03:33 -0300

    styles modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
+import {useState} from 'react';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import Modal from 'react-modal';
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+Modal.setAppElement ('#root');
 
 
 export function App() {
+
+const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+function handleOpenNewTransactionModal () {
+setIsNewTransactionModalOpen (true);
+}
+
+function handleClosedNewTransactionModal () {
+setIsNewTransactionModalOpen (false);
+}
   return (
     <>
-   <Header />
-   <GlobalStyle />
-   <Dashboard />
+   <Header  onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
    
+   <NewTransactionModal 
+   isOpen={isNewTransactionModalOpen}
+   onRequestClose={handleClosedNewTransactionModal}
+   
+   
+   />
+   <Dashboard />
+   <GlobalStyle />
     </>
   );
 }
